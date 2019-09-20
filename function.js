5b152f1f79d8b9c4aed86d8815bfb0950741cd57
kunal015 <43533375+kunal015@users.noreply.github.com>
2019-09-20 21:51:49 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,19 @@
+function myfunction()
+{
+	document.getElementById("mydiv").style.display="block";
+}
+function myfunction1()
+{
+	document.getElementById("mydiv").style.display="none";
+}
+function myfunction2()
+{
+	window.print();
+}
+function myfunction3()
+{
+	if(confirm("Are You Sure"))
+	{
+		window.close();
+	}
+}
